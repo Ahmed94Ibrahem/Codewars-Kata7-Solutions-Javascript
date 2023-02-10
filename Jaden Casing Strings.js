02c17c8f924f0e269436e4bccc5e6c63eafe649b
Ahmed94Ibrahem <ahmedibrahema061@gmail.com>
2023-02-10 09:24:55 +0200

    test

--- Jaden Casing Strings.js
+++ Jaden Casing Strings.js
@@ -16,5 +16,5 @@
 String.prototype.toJadenCase = function () {
     return this.split(' ').map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
 }
-let str = 'How can mirrors be real if our eyes aren\'t real'
+let str = 'How can mirrors be real if our eyes aren\'t real';
 console.log(str.toJadenCase());
